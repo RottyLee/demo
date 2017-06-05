3903385ac833e40d2b868708264fe82e16094338
ljinqi <ljinqi@rolaner.com>
2017-06-05 14:48:27 +0800

    测试项目

---
+++ hisheng/dir/README.md
@@ -0,0 +1,2 @@
+# composer require  hisheng/dir
+## 一个遍历 某个路径 path 下面的所有文件库
---
+++ hisheng/dir/test/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hisheng
+ * Date: 2017/6/5
+ * Time: 11:40
+ */
+require './../vendor/autoload.php';
+$dir = new \Dir\Dir();
+
+$files = $dir->path("D:/zimu/srts/unsaved")->gets();
+//var_dump($files);
+//
+//foreach ($files as $file){
+//    var_dump(file_get_contents($file));
+//}
